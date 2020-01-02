02dd35152cc37198a3d30bb821e44d2342535293
sosunnyproject <parksosun1103@gmail.com>
2020-01-02 22:15:57 +0900

    tutorial done

--- 2.JavascriptGameApp/app.js
+++ 2.JavascriptGameApp/app.js
@@ -3,8 +3,15 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode"); // fill button
+const saveBtn = document.getElementById("jsSave"); // fill button
 
-ctx.strokeStyle = "#2c2c2c";
+
+const INITIAL_COLOR = "#2c2c2c";
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 
@@ -37,11 +44,26 @@ function onMouseMove(event) {
     }
 }
 
+function handleCanvasClick() {
+    if (filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+// right click - no default menu appears
+// instead, you want users to press SAVE button
+function handleCM(event){
+    console.log(event);
+    event.preventDefault();
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM); 
 }
 
 // change colors 
@@ -53,6 +75,7 @@ function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     // console.log(color);
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 Array.from(colors).forEach(color => 
@@ -73,15 +96,36 @@ if (range) {
 // fill the entire canvas color
 // change the button text between fill and paint
 function handleModeClick(){
+
     if(filling === true) {
         filling = false;
         mode.innerText = "Fill";  
     } else{
         filling = true;
         mode.innerText = "Paint";  
+
     }
 }
 
 if (mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+function handleSaveClick(){
+    // get data of canvas image
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toDataURL
+    const image = canvas.toDataURL();
+    console.log(image);
+
+    // create link
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    // console.log(link);
+    link.click();
+
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
