5779c5e51b3fdb4f4f18d0b74df9e0bb0ec2825b
sosunnyproject <sosunpark@poscoict.com>
2020-01-02 18:29:58 +0900

    change color and stroke and mode

---
+++ 2.JavascriptGameApp/app.js
@@ -0,0 +1,87 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode"); // fill button
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+
+    // draw a line
+
+}
+
+function onMouseMove(event) {
+    // console.log(event);
+    // clientXandY is entire screen's x,y coord
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    if(!painting) {         // false
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {                // true
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
+
+// change colors 
+console.log(colors);  // returns HTMLCOllection
+console.log(Array.from(colors)); // returns array
+
+function handleColorClick(event){
+    // console.log(event.target.style);
+    const color = event.target.style.backgroundColor;
+    // console.log(color);
+    ctx.strokeStyle = color;
+}
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleColorClick)
+);
+
+// change strokeWeight
+function handlRangeChange(event){
+    console.log(event.target.value, event.target.valueAsNumber);
+    ctx.lineWidth = event.target.valueAsNumber;
+}
+
+
+if (range) {
+    range.addEventListener("input", handlRangeChange);
+}
+
+// fill the entire canvas color
+// change the button text between fill and paint
+function handleModeClick(){
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";  
+    } else{
+        filling = true;
+        mode.innerText = "Paint";  
+    }
+}
+
+if (mode) {
+    mode.addEventListener("click", handleModeClick);
+}
