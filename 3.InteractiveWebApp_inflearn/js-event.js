b885ac48d7dde1f60b15618798f28f321d13385d
sosunnyproject <parksosun1103@gmail.com>
2020-01-09 19:18:39 +0900

    함수 호출 방법들

--- 3.InteractiveWebApp_inflearn/js-event.js
+++ 3.InteractiveWebApp_inflearn/js-event.js
@@ -1,12 +1,21 @@
-window.addEventListener('DOMContentLoaded', function(){ 
-    // need this load, DOMContentLoaded if you add script tag insdie head tag
-    console.log("loaded");
+// 1. es6 스타일
+const kyo = document.querySelector('.kyo.c');
+const clickKyoHandler = () => {
+    console.log("clicked");
+    kyo.classList.toggle('special');
+}
+
+kyo.addEventListener('click', clickKyoHandler);
+
+
+// 2. 전역 아닌 지역 변수로 함수 생성
+(function() {
     const kyo = document.querySelector('.kyo.c');
 
-    function clickKyoHandler(){
+    function clickKyoHandler2() {
         console.log("clicked");
         kyo.classList.toggle('special');
     }
 
-    kyo.addEventListener('click', clickKyoHandler);
-})
+    kyo.addEventListener('click', clickKyoHandler2);
+})();
