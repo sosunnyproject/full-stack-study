0f7e7a76d141f03fb56f53be5599432d2b7c8b52
sosunnyproject <parksosun1103@gmail.com>
2020-01-14 14:34:13 +0900

    debug

--- 3.InteractiveWebApp_inflearn/js-event.js
+++ 3.InteractiveWebApp_inflearn/js-event.js
@@ -19,8 +19,14 @@ function clickHandler(event) {
     // console.log(event.target.dataset.value) // == button.getAttribute('data-value');
 
     let elem = event.target;
+    // if 대신에 while로 하면 하위의 하위여도 계속 menu-btn 부모 노드를 찾을때까지 loop 돌음.
+    // 단, menu-btn 상위를 클릭하면 에러날 수 있음.
     while(!elem.classList.contains('menu-btn')) {  // --> menu: gray area 클릭하면 에러남.
         elem = elem.parentNode;
+        if(elem.nodeName === "BODY") {             // 버튼 밖의 area 를 누르면 아무것도 실행 안하도록.
+            elem = null;
+            return;
+        }
     }
     console.log(elem.dataset.value);
 }
